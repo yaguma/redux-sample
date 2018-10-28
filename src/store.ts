11eaff1117b03fd7320fd5e14cfe5e648d3388f2
yaguma <yaguma34@gmail.com>
2018-10-28 22:42:09 +0900

    redux-logger,redux-thunk追加

--- src/store.ts
+++ src/store.ts
@@ -1,14 +1,19 @@
-import { combineReducers, createStore } from 'redux';
+import { applyMiddleware, combineReducers, createStore } from 'redux';
+import logger from 'redux-logger';
+import thunkMiddleware from 'redux-thunk';
 import { hogeAsyncReducers, IHogeState } from './states/hogeAsyncState';
+
+const reducer = combineReducers<IAppState>({
+  hoge: hogeAsyncReducers
+});
 
 export interface IAppState {
   hoge: IHogeState
 };
 
 const store = createStore(
-  combineReducers<IAppState>({
-    hoge: hogeAsyncReducers
-  })
+  reducer,
+  applyMiddleware(logger, thunkMiddleware)
 );
 
 export default store;
